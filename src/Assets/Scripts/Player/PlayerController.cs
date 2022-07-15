16af5f7fe9cc2052c00522ce4cc9065f2b60bf28
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2022-07-15 19:39:38 -0400

    added animations and faces

--- src/Assets/Scripts/Player/PlayerController.cs
+++ src/Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public Transform movePoint;
     public LayerMask collisionLayer;
+    public Animator anim;
+    bool canMove = true;
+    float delay = .5f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,25 +21,61 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-
-        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        Debug.Log(canMove);
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f&&canMove)
         {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
+                anim.SetBool("isMoving",true);
+
+                if(Input.GetAxisRaw("Horizontal") == 1f){
+                    anim.SetBool("MoveRight",true);
+                }
+                else
+                {
+                    anim.SetBool("MoveLeft",true);
+                }
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, collisionLayer))
                 {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
-
+                StartCoroutine(MoveDelay());
             }
+
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
+                if(Input.GetAxisRaw("Vertical") == 1f){
+                    anim.SetBool("MoveUp",true);
+                }
+                else
+                {
+                    anim.SetBool("MoveDown",true);
+                }
+
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, collisionLayer))
                 {
                     movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
                 }
+                StartCoroutine(MoveDelay());
             }
+    }
+    else{
+            anim.SetBool("MoveRight",false);
+            anim.SetBool("MoveLeft",false);
+            anim.SetBool("MoveUp",false);
+            anim.SetBool("MoveDown",false);
+            anim.SetBool("isMoving",false);
+        
+    }
 
-        }
+
+    }
+
+
+    private IEnumerator MoveDelay(){
+        Debug.Log("go");
+        canMove = false;
+        yield return new WaitForSeconds(delay);
+        canMove = true;
     }
 }

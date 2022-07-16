d41aef99bdcc7bab89e946aebdb7fd1cd093cab3
Ethan Ferrabelo <74121414+eferrabelo1114@users.noreply.github.com>
2022-07-15 20:42:35 -0600

    UI Stuff

--- src/Assets/Scripts/Game/LevelManager.cs
+++ src/Assets/Scripts/Game/LevelManager.cs
@@ -7,25 +7,40 @@ public class LevelManager : MonoBehaviour
     private GameManager gameManager;
     private GameObject player;
     private GameObject LevelSpawnpoint;
+    private CheckFace endOfLevel; 
+    private LevelLoader levelLoader;
 
+    private bool leveCompleted = false;
+
+    public string NextLevel = "1-2";
     public Transform LevelLoader;
 
     void Awake() {
-        Instantiate(LevelLoader);
-
+        levelLoader = Instantiate(LevelLoader).GetComponent<LevelLoader>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
         LevelSpawnpoint = GameObject.Find("PlayerSpawn");
-
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        endOfLevel = GameObject.Find("End").GetComponent<CheckFace>();
 
         gameManager.currentPlayerSpawnpoint = LevelSpawnpoint.transform.position;
 
         Destroy(LevelSpawnpoint);
 
         SceneHelper.LoadScene("Player", true);
+    }
+
+    void Update() {
+        if (!endOfLevel.goalMet) { return;}
+
+
+        if (!leveCompleted) {
+            Debug.Log("Level Completed");
+            levelLoader.LoadLevel(NextLevel);
+            leveCompleted = true;
+        }
     }
 }

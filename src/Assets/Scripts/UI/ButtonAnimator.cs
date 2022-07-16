23b87c12a5ff10b04705893e61ec4c6f68ce429a
Ethan Ferrabelo <74121414+eferrabelo1114@users.noreply.github.com>
2022-07-16 13:20:26 -0600

    UI Changes button and level manager

---
+++ src/Assets/Scripts/UI/ButtonAnimator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonAnimator : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+
+    public Image buttonImage;
+    public Sprite buttonDown;
+    public Sprite buttonUp;
+    public Transform buttonText;
+
+    void Start() {
+        buttonImage.sprite = buttonUp;
+        buttonText.localPosition = new Vector3(0, 3.47f, 0);
+    }
+
+    void ChangeButtonToDown() {
+        buttonImage.sprite = buttonDown;
+        buttonText.localPosition = new Vector3(0, 1.03f, 0);
+    }
+
+    void ChangeButtonToUp() {
+        buttonImage.sprite = buttonUp;
+        buttonText.localPosition = new Vector3(0, 3.47f, 0);
+    }
+
+    public void OnPointerUp(PointerEventData eventData){
+       Debug.Log("Pointer Up");
+       ChangeButtonToUp();
+    }
+
+    public void OnPointerDown(PointerEventData eventData){
+       Debug.Log("Pointer Down");
+       ChangeButtonToDown();
+    }
+}

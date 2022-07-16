8ebc852adab41b9a04f0199705572e24ca72fd07
cscopetski <cscopetski@gmail.com>
2022-07-16 15:25:58 -0400

    Update LevelLoader.cs

--- src/Assets/Scripts/Game/LevelLoader.cs
+++ src/Assets/Scripts/Game/LevelLoader.cs
@@ -9,15 +9,17 @@ public class LevelLoader : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {   
-         gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
     }
 
-    public void LoadLevel(string level) {
+    public void LoadLevel(string level)
+    {
         StartCoroutine(ChangeLevel(level));
     }
 
-    IEnumerator ChangeLevel(string level) {
+    IEnumerator ChangeLevel(string level)
+    {
         Transition.SetTrigger("Start");
 
         yield return new WaitForSeconds(1);

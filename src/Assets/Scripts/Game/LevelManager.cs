96e50209619b331bc73035dad9d728a0855a0cec
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2022-07-17 01:03:37 -0400

    Revert "Merge branch 'main' into samsbranch"
    This reverts commit 5ed074f74329e6d9ca0dfce964cafb636dd3116d, reversing
    changes made to 4532394f8998cfee76c621c185ee3521bb3c1ac6.

--- src/Assets/Scripts/Game/LevelManager.cs
+++ src/Assets/Scripts/Game/LevelManager.cs
@@ -41,12 +41,6 @@ public class LevelManager : MonoBehaviour
         {
             Instantiate(AudioManagerPrefab);
         }
-
-        // For level testing if there is a camera in the  scene
-        GameObject camera = GameObject.Find("Main Camera");
-        if (camera != null) {
-            Destroy(camera);
-        }
     }
 
     void Start()

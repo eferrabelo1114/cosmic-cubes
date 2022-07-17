04b927e7044b003c2c5793974e3d6d15d0e0ba84
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2022-07-17 13:34:19 -0400

    small fixes

--- src/Assets/Scripts/Game/LevelManager.cs
+++ src/Assets/Scripts/Game/LevelManager.cs
@@ -125,6 +125,7 @@ public class LevelManager : MonoBehaviour
         {
             levelLoader.LoadLevel(NextLevel);
             leveCompleted = true;
+            AudioManager.instance.PlaySound("exit!");
         }
     }
 }
